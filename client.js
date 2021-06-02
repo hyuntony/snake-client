a6c579113bdd3bbfa4a6d7cb9091e56491b3e657
hyuntony <ankim90@gmail.com>
2021-06-02 13:51:09 -0400

    added sending name string to the server

--- client.js
+++ client.js
@@ -5,9 +5,17 @@ const connect = function() {
     host: "135.23.222.131",
     port: "50542"
   });
+  conn.setEncoding("utf8");
+  // print message when connected to server
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+  // sent name string to the server
+  conn.on("connect", () => {
+    conn.write("Name: AHK");
+  });
 
   // interpret incoming data as text
-  conn.setEncoding("utf8");
   conn.on("data", (data) => {
     console.log(data);
   });
